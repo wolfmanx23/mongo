a7c3ef96429c7b067111ac1e46a6d04e274397af
Pedro Martínez Barrientos <pbarrien@gmail.com>
2021-04-28 01:05:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -72,7 +72,8 @@ let password = authentication.password;
 
 // Extract the MongoDB URIs
 let connectionPath = mongodbConn.hosts;
-let connectionString = `mongodb://${username}:${password}@${connectionPath[0].hostname}:${connectionPath[0].port},${connectionPa$
+let connectionString = `mongodb://${username}:${password}@${connectionPath[0].hostname}:${connectionPath[0].port},${connectionPath[1].hostname}:${connectionPath[1].port}/?replicaSet=replset`;
+
 
 // We want to extract the port to publish our app on
 let port = 8080;
